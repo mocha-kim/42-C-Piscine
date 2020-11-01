35a0ff9c28a8f411ca363212077c94d3f0d45518
Sunhye Kim <sunhkim@c8r2s2.42seoul.kr>
2020-11-01 16:06:06 +0900

    C05

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -6,22 +6,22 @@
 /*   By: sunhkim <jsg2804@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 18:25:05 by sunhkim           #+#    #+#             */
-/*   Updated: 2020/10/29 18:46:13 by sunhkim          ###   ########.fr       */
+/*   Updated: 2020/11/01 10:46:12 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_sqrt(int nb)
 {
-	int i;
-	int sqrt;
+	unsigned int i;
 
 	i = 1;
-	sqrt = 0;
-	if (nb <= 0)
+	if (nb < 0)
 		return (0);
-	while (i * i < nb)
+	if (nb == 1)
+		return (nb);
+	while (i * i < (unsigned int)nb)
 		i++;
-	if (i * i == nb)
+	if (i * i == (unsigned int)nb)
 		return (i);
 	else
 		return (0);
